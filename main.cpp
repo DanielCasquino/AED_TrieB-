64165f7abdcd056f85ef29623076131b2c922946
mukanjy0 <mukanjyo29@protonmail.com>
2024-02-08 23:20:47 -0500

    modified BTree to support key pointers instead of storing the key itself in each node

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include "Trie.h"
+#include "BTree.h"
 #include <cassert>
 using namespace std;
 
+// TRIE
 void test_insert_search() {
     Trie trie;
     trie.insert("hello");
@@ -30,8 +32,27 @@ void test_erase() {
     assert(trie.search("a") == false);
 }
 
+// BTree
+void test() {
+    BTree<char> bt (3);
+    bt.insert('b');
+    bt.insert('a');
+    bt.insert('c');
+    bt.insert('d');
+    bt.insert('e');
+    bt.insert('f');
+    bt.erase('f');
+    bt.erase('c');
+    bt.traverse([](char k)->void{ cout << k << ' ';});
+    assert(bt.search('a') == true);
+    assert(bt.search('e') == true);
+    assert(bt.search('f') == false);
+    assert(bt.search('c') == false);
+}
+
 int main() {
-    test_insert_search();
-    test_erase();
+//    test_insert_search();
+//    test_erase();
+    test();
     return 0;
 }
